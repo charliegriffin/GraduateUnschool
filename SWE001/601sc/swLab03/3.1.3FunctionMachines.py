6c52ac60c73d5eb50974073dc8442d44373941ca
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-08-12 13:12:03 -0700

    Completed Exercise 3.1.4 Combining Accounts

--- SWE001/601sc/swLab03/3.1.3FunctionMachines.py
+++ SWE001/601sc/swLab03/3.1.3FunctionMachines.py
@@ -3,5 +3,5 @@ import lib601.sm as sm
 class PureFunction(sm.SM):
 	def __init__(self, f):
 		self.f = f
-	def getNextValues(self,state,inp)
+	def getNextValues(self,state,inp):
 		return (state,self.f(inp))
