fda99c456e1f009534a938cf45e8d9eb23dca724
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-09-15 20:48:56 -0700

    Finished Task 2
    my error was due to not commenting out test code

--- SWE001/6.02s2009/labs/lab1ClockAndDataRecovery/lab1_2.py
+++ SWE001/6.02s2009/labs/lab1ClockAndDataRecovery/lab1_2.py
@@ -10,7 +10,7 @@ def v_samples_to_d_samples(samples):
     Convert an array of voltage samples into an array of
     digitized samples.
     """
-    return (samples < 0.2)*1
+    return (samples > 0)*1
 
 def find_center_of_start_bit(d_samples,start,samples_per_bit):
     """
@@ -18,12 +18,12 @@ def find_center_of_start_bit(d_samples,start,samples_per_bit):
     START bit returning -1 if no start bit is found.
     """
     position = start		# code assumes start bit is a 0
-    while (position < len(d_samples)):
-    	if d_samples[position] == 1:
-    		return position + samples_per_bit/2
-    	else:
-    		position += samples_per_bit
-    return -1
+    while d_samples[position] == 0:
+		position += 1
+		if position >= len(d_samples)-samples_per_bit/2:
+			return -1 
+    
+    return position + samples_per_bit/2
 
 def decode_data(d_samples,start,nbits,samples_per_bit):
     """
@@ -50,7 +50,6 @@ def receive(samples):
     message = []
     start = 0
     
-    start = find_center_of_start_bit(d_samples,start,8)
     while True:
         # locate sample at middle of next START bit
         start = find_center_of_start_bit(d_samples,start,8)
@@ -59,18 +58,17 @@ def receive(samples):
 
         # grab the eight data bits which follow
         bits = decode_data(d_samples,start+8,8,8)
-        print bits
-# 
-#         # first convert bit sequence to an int
-#         # and then to a character, append to message
-#         message.append(chr(lab1.bits_to_int(bits)))
-# 
-#         # finally skip to the middle of the STOP bit
-#         # and start looking for the next START bit
-#         start += 9*8
-# 
-#     # join all the message characters into a single string
-#     return "".join(message)
+#         print bits
+
+        # first convert bit sequence to an int
+        # and then to a character, append to message
+        message.append(chr(lab1.bits_to_int(bits)))
+        # finally skip to the middle of the STOP bit
+        # and start looking for the next START bit
+        start += 9*8
+
+    # join all the message characters into a single string
+    return "".join(message)
 
 
 # testing code.  Do it this way so we can import this file
@@ -78,3 +76,4 @@ def receive(samples):
 if __name__ == '__main__':
     # see if we can decode the message
     lab1.task2_test(receive,'hi there')
+    lab1.task2_test(receive,'pizza boy')
