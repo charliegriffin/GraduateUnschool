2716545b2356c5a383ca534aaea9de2f024b6d11
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-09-18 16:02:52 -0700

    Translated a two word message

--- SWE001/6.02s2009/labs/lab1ClockAndDataRecovery/lab1_3.py
+++ SWE001/6.02s2009/labs/lab1ClockAndDataRecovery/lab1_3.py
@@ -25,7 +25,7 @@ def find10BitSequence(recievedSamples):
 	for i in range(len(recievedSamples) - 10):
 		currentSequence = numpy.array(recievedSamples[i:i+10])
 		if numpy.array_equal(currentSequence,startSequence1) or numpy.array_equal(currentSequence,startSequence2):
-			return recievedSamples[i+10:i+20]
+			return i+10
 
 def receive_8b10b(samples):
 	"""
@@ -33,18 +33,28 @@ def receive_8b10b(samples):
 	8b/10b encoder into a message string.
 	"""
 	# digitize the incoming voltage samples
+	message = ''
 	R = 8 # number of received samples per bit
 	d_samples = lab1_2.v_samples_to_d_samples(samples)
 	recievedSamples = digitizedToRecieved(d_samples,R)
 # 	print recievedSamples
-	sequence = find10BitSequence(recievedSamples)
+	startPosition = find10BitSequence(recievedSamples)
+	sequence = numpy.array(recievedSamples[startPosition:startPosition+10])
+# 	sequence = find10BitSequence(recievedSamples)
+	print sequence
 	tenBitIndex = lab1.bits_to_int(sequence)
+	print tenBitIndex
 	eightBitIndex = lab1.table_10b_8b[tenBitIndex]
-	return chr(eightBitIndex)
+	message += chr(eightBitIndex)
+	sequence2 = numpy.array(recievedSamples[startPosition+10:startPosition+20])
+	tenBitIndex2 = lab1.bits_to_int(sequence2)
+	eightBitIndex2 = lab1.table_10b_8b[tenBitIndex2]
+	message += chr(eightBitIndex2)
+	return message
 
 # testing code.  Do it this way so we can import this file
 # and use its functions without also running the test code.
 if __name__ == '__main__':
     # supply some test data....
-    lab1.task3_test(receive_8b10b,"y")
+    lab1.task3_test(receive_8b10b,"yb")
 #     lab1.task3_test(receive_8b10b,lab1.long_message)
