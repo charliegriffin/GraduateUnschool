375a336765537fad680725f7da41c53ed5aa2a83
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-09-22 17:21:31 -0700

    Unstuffed the bits

--- SWE001/6.02s2009/labs/lab4/lab4_1.py
+++ SWE001/6.02s2009/labs/lab4/lab4_1.py
@@ -7,22 +7,30 @@ def decode(received):
     # blocks, each element of the list is itself a list
     # of 0's and 1's that make up that particular block.
     for block in lab4.find_sync(received,lab4.sync):
-
         # undo the bit-stuffing (remove 0 following seven
         # consecutive 1's)
-        ... your code here ...
+        stuffedBit = [1,1,1,1,1,1,1]
+        indexesToUnstuff = []
+        for index in range(len(block)-7): # find the locations of the stuffed bits
+        	if block[index:index+7] == stuffedBit:
+        		indexesToUnstuff.append(index+8)
+        for i in range(len(indexesToUnstuff)):	# remove the stuffed bits
+        	del block[i]
+        	if i == len(indexesToUnstuff) - 1:
+        		break
+        	else:
+        		indexesToUnstuff[i+1] -= 1		# update the indexes to reflect the changes we made
+		# now all blocks are 15*16 in length
 
         # now block should have exactly 15*16 elements.
         # use lab4.interleave to deinterleave the block.
-        ... your code here ...
+
 
         # for each group of 15 bits in the block, extract
         # the (uncorrected) data bits and use lab4.bin2char
         # to convert them to an ASCII character, which you
         # should append to message
-        ... your code here ...
-
-    lab4.printmsg(message)
+# 	lab4.printmsg(message)
 
 if __name__ == '__main__':
     decode(lab4.message)
