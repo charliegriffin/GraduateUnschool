72b00e84735339a79c0a1913537fc9902a5e1cf3
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-12-09 15:48:20 -0700

    solved simplest cases using unidirectional bfs

--- SWE001/6.006f2011/ps6/rubik/solver.py
+++ SWE001/6.006f2011/ps6/rubik/solver.py
@@ -8,4 +8,34 @@ def shortest_path(start, end):
     You can use the rubik.quarter_twists move set.
     Each move can be applied using rubik.perm_apply
     """
-    raise NotImplementedError
+    level = {start:0}
+    parent = {start:None}
+    i = 1
+    frontier = [start]
+    solution = []
+    while end not in level:              # explore graph
+#         print frontier
+        next = []
+        for u in frontier:
+            for move in rubik.quarter_twists:
+                v = rubik.perm_apply(move,u)
+                if v == end:
+                	print 'found a solution\t length =',i
+                if v not in level:
+#                     print 'currentPos = ',currentPos
+                    level[v] = i
+                    parent[v] = (u,move)
+                    next.append(v)
+        frontier = next
+        i += 1
+    newEnd = end
+    for i in range(level[end]):
+         solution.append(parent[end][1])
+         newEnd = solution[-1]
+    print solution
+    soultion = reversed(solution)
+    print solution
+    return solution
+
+#     if start == end:
+#     	return solution
