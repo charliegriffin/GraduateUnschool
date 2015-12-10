69952d58c19eee78303288302a13d6554d01afd5
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2015-12-10 00:05:05 -0700

    cleaned up and finished the code
    really proud of what I’ve accomplished today :)

--- SWE001/6.006f2011/ps6/rubik/solver.py
+++ SWE001/6.006f2011/ps6/rubik/solver.py
@@ -20,7 +20,7 @@ def shortest_path(start, end):
     connections = []
     while (not connected) and (i <= 7):              # explore graph
         next = []
-        for u in frontier:
+        for u in frontier:                      # move forwards
             for move in rubik.quarter_twists:
                 v = rubik.perm_apply(move,u)
                 if v not in level:
@@ -29,7 +29,7 @@ def shortest_path(start, end):
                     next.append(v)
         frontier = next
         backNext = []
-        for u in backFrontier:
+        for u in backFrontier:                  # move backwards
             for move in rubik.quarter_twists:
                 v = rubik.perm_apply(move,u)
                 if v not in backLevel:
@@ -37,27 +37,26 @@ def shortest_path(start, end):
                     backParent[v] = (u,rubik.perm_inverse(move))
                     backNext.append(v)
         backFrontier = backNext
-        for config in level.keys():
+        for config in level.keys():             # check for a solution
             if config in backLevel:
                 connected = True
                 connections.append(config)
         i += 1
-    if not connected:      # we have explored all options and found no solution
+    if not connected:  # we have explored all options and found no solution
         return None
     minConn = findShortestPath(connections,level,backLevel)
-    # join the partial solutions
+    buildSolution(level,parent,minConn,solution,'forwards')
+    buildSolution(backLevel,backParent,minConn,solution,'backwards')
+    return solution
+
+def buildSolution(level,parent,minConn,solution,direction):
     newCenter = minConn
     for i in range(level[minConn]):
         (pos,move) = parent[newCenter]
         solution.append(move)
         newCenter = pos
-    solution.reverse()
-    newCenter = minConn
-    for i in range(backLevel[minConn]):
-        (pos,move) = backParent[newCenter]
-        solution.append(move)
-        newCenter = pos
-    return solution
+    if direction == 'forwards':
+        solution.reverse()
 
 def findShortestPath(connections,level,backLevel):
     minDistance = 15 # this serves as an infinity
