fc868ff07b30be43d5d501810d5f4e459d66139d
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2017-02-05 11:52:45 -0600

    memorized search
    returning paths that are too long. Fixing this bug

--- SWE001/6.006f2011/ps7/resizeable_image.py
+++ SWE001/6.006f2011/ps7/resizeable_image.py
@@ -8,6 +8,7 @@ class ResizeableImage(imagematrix.ImageMatrix):
     def best_seam(self):
         print "best seam"
         print "dimensions = " + str(self.height) + "x" + str(self.width)
+        self.exploredPath = {}
 #         self.seamEnergy = {}
 #         self.seamPath = {}
 #         self.visit = {}
@@ -19,20 +20,43 @@ class ResizeableImage(imagematrix.ImageMatrix):
 #         for i in range(self.width):
 #             minSeams += [(i,self.height-1] # init across each horiz pixel
 #             print self.energy(i, self.height-1)
-        print self.findMinSeam(self.width/2,(self.height-1)/6)
+        (self.minSeamEnergy,self.minSeam) = self.findMinSeam(0,3)#(self.height-1))
+        print self.minSeam
+#         for i in range(1,5):#self.width):
+#             (pathEnergy,path) = self.findMinSeam(i,3)#(self.height-1))
+#             if pathEnergy < self.minSeamEnergy:
+#                 self.minSeamEnergy = pathEnergy
+#                 self.minSeam = path
+#             print path
+        print len(self.minSeam), self.height
+        print self.minSeam
+        return self.minSeam
 
     def remove_best_seam(self):
         self.remove_seam(self.best_seam())
 
     def findMinSeam(self,i,j):
 #         print i,j,self.energy(i,j)
-        if j==0:
-            return (self.energy(i,j),[(i,j)]) # return i,j too so you know what's up
-        else:
-            potentialSubSeams = []
-            for k in [-1,0,1]:
-                potentialSubSeams.append(self.findMinSeam(i+k,j-1))
-            (pathEnergy,currentPath) = min(potentialSubSeams,key=lambda item:item[0])
-            currentPath.append((i,j))
-            return (pathEnergy + self.energy(i,j),currentPath)
+        if (i,j) in self.exploredPath.keys():  # already explored
+#             print i,j,"explored"
+            return self.exploredPath[(i,j)]
+        else:                                  # not explored
+			if j==0:
+				self.exploredPath[(i,j)] = (self.energy(i,j),[(i,j)])
+				return (self.energy(i,j),[(i,j)]) # return i,j too so you know what's up
+			else:
+				potentialSubSeams = []
+				for k in [-1,0,1]:
+					if i+k >= 0 and i+k <= (self.width-1):
+						potentialSubSeams.append(self.findMinSeam(i+k,j-1))
+				(pathEnergy,currentPath) = min(potentialSubSeams,key=lambda item:item[0])
+# 				print "appending",i,j,"to current path"
+# 				print currentPath[-1][1],j
+				if currentPath[-1][1] == j:    #this shouldn't happen
+					print currentPath,i,j    # how did a path this long end up here?
+					print potentialSubSeams
+				currentPath.append((i,j))
+				pathEnergy += self.energy(i,j)
+				self.exploredPath[(i,j)] = (pathEnergy,currentPath)
+				return (pathEnergy,currentPath)
             
