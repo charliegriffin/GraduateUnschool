e52b3bd998d4cf34cfa8f2a1ce9e78362525e2ab
Charles Griffin <falconvoice.charliegriffin@gmail.com>
2017-02-05 13:09:13 -0600

    Fixed a list copying error
    The length of the path is still incorrect

--- SWE001/6.006f2011/ps7/resizeable_image.py
+++ SWE001/6.006f2011/ps7/resizeable_image.py
@@ -20,10 +20,10 @@ class ResizeableImage(imagematrix.ImageMatrix):
 #         for i in range(self.width):
 #             minSeams += [(i,self.height-1] # init across each horiz pixel
 #             print self.energy(i, self.height-1)
-        (self.minSeamEnergy,self.minSeam) = self.findMinSeam(0,3)#(self.height-1))
+        (self.minSeamEnergy,self.minSeam) = self.findMinSeam(15,5)#(self.height-1))
         print self.minSeam
-#         for i in range(1,5):#self.width):
-#             (pathEnergy,path) = self.findMinSeam(i,3)#(self.height-1))
+#         for i in range(1,self.width):
+#             (pathEnergy,path) = self.findMinSeam(i,(self.height-1))
 #             if pathEnergy < self.minSeamEnergy:
 #                 self.minSeamEnergy = pathEnergy
 #                 self.minSeam = path
@@ -36,27 +36,27 @@ class ResizeableImage(imagematrix.ImageMatrix):
         self.remove_seam(self.best_seam())
 
     def findMinSeam(self,i,j):
-#         print i,j,self.energy(i,j)
+#         print "calling with",(i,j)#,self.energy(i,j)
         if (i,j) in self.exploredPath.keys():  # already explored
-#             print i,j,"explored"
             return self.exploredPath[(i,j)]
         else:                                  # not explored
-			if j==0:
-				self.exploredPath[(i,j)] = (self.energy(i,j),[(i,j)])
-				return (self.energy(i,j),[(i,j)]) # return i,j too so you know what's up
-			else:
-				potentialSubSeams = []
-				for k in [-1,0,1]:
-					if i+k >= 0 and i+k <= (self.width-1):
-						potentialSubSeams.append(self.findMinSeam(i+k,j-1))
-				(pathEnergy,currentPath) = min(potentialSubSeams,key=lambda item:item[0])
-# 				print "appending",i,j,"to current path"
-# 				print currentPath[-1][1],j
-				if currentPath[-1][1] == j:    #this shouldn't happen
-					print currentPath,i,j    # how did a path this long end up here?
-					print potentialSubSeams
-				currentPath.append((i,j))
-				pathEnergy += self.energy(i,j)
-				self.exploredPath[(i,j)] = (pathEnergy,currentPath)
-				return (pathEnergy,currentPath)
+            if j==0:
+                self.exploredPath[(i,j)] = (self.energy(i,j),[(i,j)])
+                return (self.energy(i,j),[(i,j)]) # return i,j too so you know what's up
+            else:
+                potentialSubSeams = []
+                for k in [-1,0,1]:
+                    if i+k >= 0 and i+k <= (self.width-1):
+                        potentialSubSeams.append(self.findMinSeam(i+k,j-1))
+                (pathEnergy,currentPath) = min(potentialSubSeams,key=lambda item:item[0])
+                if currentPath[-1][1] == j:    #this shouldn't happen
+                    print "ERROR, path includes current row",currentPath,i,j    # how did a path this long end up here?
+                    print potentialSubSeams
+                currentPath.append((i,j))
+                if len(currentPath) > j+1:    # this should happen before the other error
+                    print "ERROR, path is longer than current height"
+                    print "j =",j,"path =",currentPath
+                pathEnergy += self.energy(i,j)
+                self.exploredPath[(i,j)] = (pathEnergy,currentPath[:])
+                return (pathEnergy,currentPath[:])
             
