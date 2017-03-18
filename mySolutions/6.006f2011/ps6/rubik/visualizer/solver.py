405c9ef356e31dbbf07db4e51eb7ce3c96e30f79
Charlie Griffin <charliegriffin@email.arizona.edu>
2017-03-17 19:13:39 -0500

    collect images for synthesis article

--- mySolutions/6.006f2011/ps6/rubik/visualizer/solver.py
+++ mySolutions/6.006f2011/ps6/rubik/visualizer/solver.py
@@ -9,29 +9,60 @@ def shortest_path(start, end):
     Each move can be applied using rubik.perm_apply
     """
     level = {start:0}
+    backLevel = {end:0}
     parent = {start:None}
+    backParent = {end:None}
     i = 1
     frontier = [start]
     solution = []
-    while end not in level:              # explore graph
-#         print frontier
+    backFrontier = [end]
+    connected = False
+    connections = []
+    while (not connected) and (i <= 7):              # explore graph
         next = []
-        for u in frontier:
+        for u in frontier:                           # move forwards
             for move in rubik.quarter_twists:
                 v = rubik.perm_apply(move,u)
-                if v == end:
-                	print 'found a solution'
                 if v not in level:
-#                     print 'currentPos = ',currentPos
                     level[v] = i
                     parent[v] = (u,move)
                     next.append(v)
         frontier = next
-    newEnd = end
-    for i in range(level[end]):
-         solution.append(parent[end][1])
-         newEnd = solution[-1]
+        backNext = []
+        for u in backFrontier:                      # move backwards
+            for move in rubik.quarter_twists:
+                v = rubik.perm_apply(move,u)
+                if v not in backLevel:
+                    backLevel[v] = i
+                    backParent[v] = (u,rubik.perm_inverse(move))
+                    backNext.append(v)
+        backFrontier = backNext
+        for config in level.keys():                 # check for a solution
+            if config in backLevel:
+                connected = True
+                connections.append(config)
+        i += 1
+    if not connected:  # we have explored all options and found no solution
+        return None
+    minConn = findShortestPath(connections,level,backLevel)
+    buildSolution(level,parent,minConn,solution,'forwards')
+    buildSolution(backLevel,backParent,minConn,solution,'backwards')
     return solution
 
-#     if start == end:
-#     	return solution
+def buildSolution(level,parent,minConn,solution,direction):
+    newCenter = minConn
+    for i in range(level[minConn]):
+        (pos,move) = parent[newCenter]
+        solution.append(move)
+        newCenter = pos
+    if direction == 'forwards':
+        solution.reverse()
+
+def findShortestPath(connections,level,backLevel):
+    minDistance = 15 # this serves as an infinity
+    for conn in connections:
+        distance = level[conn] + backLevel[conn]
+        if distance < minDistance:
+            minConn = conn
+            minDistance = distance
+    return minConn
